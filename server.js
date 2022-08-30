167539f729067791f699b3ae4166f773d0fe2fbd
mvpc-11232 <mvp2612@hotmail.com>
2022-08-30 09:10:19 +0530

    Auto Push Commit

--- server.js
+++ server.js
@@ -149,22 +149,25 @@ app.get('/search/:text', (req, res) => {
 
 // Using DocumentDB
 
-if (process.env.MONGO == 'true') {
-// set up Mongo
 function mongoConnect() {
     return new Promise((resolve, reject) => {
-        var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb:27017/catalogue';
-        mongoClient.connect(mongoURL, (error, client) => {
-            if(error) {
-                reject(error);
-            } else {
-                db = client.db('catalogue');
-                collection = db.collection('products');
-                resolve('connected');
-            }
-        });
-    });
-}
+    var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
+    var client = mongoClient.connect(mongoURL,
+      {
+        // Mutable & Immutable
+        //tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+        // Container
+        tlsCAFile: `/home/roboshop/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+    }, (error, client) => {
+    if(error) {
+        reject(error);
+    } else {
+        db = client.db('catalogue');
+        collection = db.collection('products');
+        resolve('connected');
+    }
+});
+});
 }
 
 if (process.env.DOCUMENTDB == 'true') {
