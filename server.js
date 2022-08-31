665a051dcf1d7fd8831ae3fc27ca98684b4df0be
mvpc-11232 <mvp2612@hotmail.com>
2022-08-31 23:29:47 +0530

    Auto Push Commit

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ app.get('/search/:text', (req, res) => {
 
 function mongoConnect() {
     return new Promise((resolve, reject) => {
-    var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
+    var mongoURL = process.env.MONGO_URL || 'mongodb://admin1:roboshop1@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
     var client = mongoClient.connect(mongoURL,
       {
         // Mutable & Immutable
