3e56f27dd0c9dd6e04cc4821e51a3cb1533475cb
mvpc-11232 <mvp2612@hotmail.com>
2022-08-31 23:26:52 +0530

    Auto Push Commit

--- server.js
+++ server.js
@@ -170,28 +170,28 @@ function mongoConnect() {
 });
 }
 
-if (process.env.DOCUMENTDB == 'true') {
-function mongoConnect() {
-    return new Promise((resolve, reject) => {
-    var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
-    var client = mongoClient.connect(mongoURL,
-      {
-        // Mutable & Immutable
-        //tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
-        // Container
-        tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
-    }, (error, client) => {
-    if(error) {
-        reject(error);
-    } else {
-        db = client.db('catalogue');
-        collection = db.collection('products');
-        resolve('connected');
-    }
-});
-});
-}
-}
+// if (process.env.DOCUMENTDB == 'true') {
+// function mongoConnect() {
+//     return new Promise((resolve, reject) => {
+//     var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
+//     var client = mongoClient.connect(mongoURL,
+//       {
+//         // Mutable & Immutable
+//         //tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+//         // Container
+//         tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+//     }, (error, client) => {
+//     if(error) {
+//         reject(error);
+//     } else {
+//         db = client.db('catalogue');
+//         collection = db.collection('products');
+//         resolve('connected');
+//     }
+// });
+// });
+// }
+// }
 
 
 // mongodb connection retry loop
